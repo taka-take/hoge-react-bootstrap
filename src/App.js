1bd28e0475f6773e927833cdc7136c1df362fecd
takatake <taka12hiro01.o.m@gmail.com>
2020-10-22 19:55:04 +0900

    remove body

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <body>
-        <Sheet />
-      </body>
+      <Sheet />
     </div>
   );
 }
